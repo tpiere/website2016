7285224324012821c45226d13b24dbda8192f454
Tom Piere <tpiere@gmail.com>
2016-12-12 00:25:09 -0600

    moved menu

--- styles.5f7f16a82a4f91544984.bundle.js
+++ styles.5f7f16a82a4f91544984.bundle.js
@@ -1,2 +1,2 @@
-webpackJsonp([2,3],{437:function(n,o){},438:function(n,o){},439:function(n,o){},793:function(n,o,c){c(437),c(439),n.exports=c(438)}},[793]);
+webpackJsonp([2,3],{452:function(n,o){},453:function(n,o){},454:function(n,o){},805:function(n,o,c){c(452),c(454),n.exports=c(453)}},[805]);
 //# sourceMappingURL=styles.5f7f16a82a4f91544984.bundle.map
