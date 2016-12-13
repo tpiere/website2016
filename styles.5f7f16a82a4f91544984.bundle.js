c2c6ec4bead862e0c5ca53955ed0177c1bfddf03
Tom Piere <tpiere@gmail.com>
2016-12-12 19:09:46 -0600

    hiding menu on home page

--- styles.5f7f16a82a4f91544984.bundle.js
+++ styles.5f7f16a82a4f91544984.bundle.js
@@ -1,2 +1,2 @@
-webpackJsonp([2,3],{452:function(n,o){},453:function(n,o){},454:function(n,o){},805:function(n,o,c){c(452),c(454),n.exports=c(453)}},[805]);
+webpackJsonp([2,3],{454:function(n,o){},455:function(n,o){},456:function(n,o){},809:function(n,o,c){c(454),c(456),n.exports=c(455)}},[809]);
 //# sourceMappingURL=styles.5f7f16a82a4f91544984.bundle.map
